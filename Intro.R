9fa35b1851de605255be65eefd9f699afb2bdf74
Kalicz Péter <kalicz.peter@uni-sopron.hu>
2025-09-03 14:59:07 +0200

    Első műveletek

---
+++ Intro.R
@@ -0,0 +1,7 @@
+a = 1 # a nevű objektum létrehozása
+a <- 1 # ugyan az, de az R-es hozzárendelő operátorral
+2 > 1 # logikai, relációs operátorok is vannak, mint a matekban eredménye TRUE vagy FALSE
+a # obejtum nevet beírva nézhetem az objektum tartalmát
+b <- c(12.1,12.5,11.9,13.1,12.9,11) # b objektum több adattal
+b # az új vektor tartalmának kiírása
+plot(b) # a fenti adatok megjelenítése az index alapján
