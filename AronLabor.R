f6fd535ae4cb344ffc8b15727327c992d4ec95c1
Péter Kalicz <kaliczp@gmail.com>
2025-10-21 20:51:53 +0200

    Row fixing and barplot

--- AronLabor.R
+++ AronLabor.R
@@ -2,7 +2,14 @@ library(readxl) # Csomag betöltés
 NAronszennyviz <- as.data.frame(read_excel("NAronszennyviz.xlsx")) # teszt beolvasás
 
 
-SzVTerület <- NAronszennyviz[27:45,] # Terület kiválsztása
+SzVTerület <- NAronszennyviz[28:45,] # Terület kiválsztása
+Fejléchez <- as.vector(t(NAronszennyviz[26,]))
+Fejléchez[2] <- "Loc" # Hely neve
+names(SzVTerület) <- Fejléchez # Fejléc
+
+Standard <- NAronszennyviz[27,] # Határ
 names(SzVTerület) <- as.vector(t(NAronszennyviz[26,])) # Fejléc
 
-plot(SzVTerület[,1], SzVTerület[,3])
+
+barplot(as.numeric(SzVTerület[,3]), log = "y")
+axis(2, at = as.numeric(Standard[,3]), tck = 1, lab = "")
