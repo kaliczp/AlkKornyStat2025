6ed6c6057dd9867e3abcf0e363941564b5d7e11b
Péter Kalicz <kaliczp@gmail.com>
2025-10-21 20:19:57 +0200

    Comment area selection

--- AronLabor.R
+++ AronLabor.R
@@ -1,5 +1,6 @@
 library(readxl) # Csomag betöltés
 NAronszennyviz <- as.data.frame(read_excel("NAronszennyviz.xlsx")) # teszt beolvasás
 
-SzVTerület <- NAronszennyviz[27:45,]
-names(SzVTerület) <- as.vector(t(NAronszennyviz[26,]))
+
+SzVTerület <- NAronszennyviz[27:45,] # Terület kiválsztása
+names(SzVTerület) <- as.vector(t(NAronszennyviz[26,])) # Fejléc
