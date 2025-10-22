039e8c6328df8acbb0c9d60a9c635c07b5091965
Péter Kalicz <kaliczp@gmail.com>
2025-10-22 15:26:14 +0200

    Változónevek használata a standardhoz

--- AronLabor.R
+++ AronLabor.R
@@ -8,8 +8,7 @@ Fejléchez[2] <- "Loc" # Hely neve
 names(SzVTerület) <- Fejléchez # Fejléc
 
 Standard <- NAronszennyviz[27,] # Határ
-names(SzVTerület) <- as.vector(t(NAronszennyviz[26,])) # Fejléc
-
+names(Standard) <- Fejléchez # Fejléc
 
 barplot(as.numeric(SzVTerület[,3]), log = "y")
 axis(2, at = as.numeric(Standard[,3]), tck = 1, lab = "")
