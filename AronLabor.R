773c766537869098217cb90bd3c62a5af384d816
Péter Kalicz <kaliczp@gmail.com>
2025-10-22 15:29:46 +0200

    Cimkék igazítása

--- AronLabor.R
+++ AronLabor.R
@@ -10,7 +10,7 @@ names(SzVTerület) <- Fejléchez # Fejléc
 Standard <- NAronszennyviz[27,] # Határ
 names(Standard) <- Fejléchez # Fejléc
 
-par(mar=c(8,4.1,0.3,0.3), las = 2)
+par(mar=c(10,4.1,0.3,0.3), las = 2)
 barplot(as.numeric(SzVTerület[,3]), log = "y")
 axis(2, at = as.numeric(Standard[,3]), tck = 1, lab = "")
 axis(1, at = seq(0.7, by = 1.2, length.out = nrow(SzVTerület)), labels = SzVTerület[,2])
